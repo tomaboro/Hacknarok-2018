69a6e52bb1844fdcf32dd6bb382eb4251b17393b
tomaboro <motek@student.agh.edu.pl>
2018-03-10 13:47:06 +0100

    initial commit

---
+++ src/main/java/pl/hacknarok/positivedevs/runit/RunitApplication.java
@@ -0,0 +1,12 @@
+package pl.hacknarok.positivedevs.runit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunitApplication.class, args);
+	}
+}
